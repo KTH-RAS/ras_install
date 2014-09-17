04caee8e69bb438c98738e35b0f88de846709b0b
fevb <fevb@kth.se>
2014-09-17 11:15:01 +0200

    added phidgets install script

---
+++ scripts/install_phidgets_imu.sh
@@ -0,0 +1,10 @@
+cd ~/catkin_ws/src
+wget https://raw.githubusercontent.com/KTH-RAS/ras_install/hydro-2014/rosinstall/imu.rosinstall
+
+wstool merge imu.rosinstall -y
+wstool update phidgets_drivers imu_tools 
+
+cd ..
+catkin_make
+
+rosrun phidgets_api setup-udev.sh
