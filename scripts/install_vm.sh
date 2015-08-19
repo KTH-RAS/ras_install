a9257793c87bfa7ba2eb1108e64ee6cee1556e53
Francisco Vina <fevb@kth.se>
2015-08-19 13:38:19 +0200

    switched install scripts to indigo

--- scripts/install_vm.sh
+++ scripts/install_vm.sh
@@ -2,27 +2,28 @@
 
 # install ros
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
 
 sudo apt-get update
-sudo apt-get install ros-hydro-desktop -y
+sudo apt-get install ros-indigo-desktop -y
 
 sudo rosdep init
 rosdep update
 
+
 # setup environment
-source /opt/ros/hydro/setup.bash
+source /opt/ros/indigo/setup.bash
 echo "export EDITOR=emacs" >> ~/.bashrc
 
 # install editors/ ssh
 sudo apt-get install ssh emacs qtcreator vim -y
 
 # install ros packages and dependencies
-sudo apt-get install ros-hydro-rqt-graph ros-hydro-rqt-gui ros-hydro-rqt-plot ros-hydro-kobuki-soft ros-hydro-kobuki-keyop ros-hydro-roscpp-tutorials -y
+sudo apt-get install ros-indigo-rqt-graph ros-indigo-rqt-gui ros-indigo-rqt-plot ros-indigo-kobuki-soft ros-indigo-kobuki-keyop ros-indigo-roscpp-tutorials -y
 
-sudo apt-get install libboost-random1.46-dev -y
+sudo apt-get install libboost-random1.55-dev -y
 
 
 # pip wstool and git
@@ -46,7 +47,7 @@ source ~/.bashrc
 
 # merge rosinstall file
 cd ~/catkin_ws/src
-wget https://raw.githubusercontent.com/KTH-RAS/ras_install/hydro-2014/rosinstall/lab1.rosinstall
+wget https://raw.githubusercontent.com/KTH-RAS/ras_install/indigo-2015/rosinstall/lab1.rosinstall
 
 wstool merge lab1.rosinstall
 wstool update
@@ -55,3 +56,4 @@ cd ~/catkin_ws
 catkin_make
 source ~/.bashrc
 
+sh ./install_phidgets_imu.sh
