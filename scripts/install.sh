38f864785fb509e52755bc75718d38a738e4600e
VladimirLi <vladde1991@gmail.com>
2016-09-20 17:34:26 +0200

    fix wstool link

--- scripts/install.sh
+++ scripts/install.sh
@@ -69,7 +69,7 @@ cd
 sudo pip install pyuarm
 cd ~/catkin_ws/src
 
-wget https://raw.githubusercontent.com/KTH-RAS/ras_install/indigo-2016/rosinstall/ras_utils.rosinstall
+wget https://raw.githubusercontent.com/KTH-RAS/ras_install/test-2016/rosinstall/ras_utils.rosinstall
 wstool merge ras_utils.rosinstall
 wstool update
 rosdep install --skip-keys=librealsense --from-paths -i realsense/realsense_camera/src/
