4b25b502a32407a3fb24d53c47bd13cd21254827
Diogo Almeida <diogoa@kth.se>
2018-02-12 18:08:24 +0100

    Update install scripts to modularize the basic components

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,51 +6,8 @@ echo 'APT::Periodic::Update-Package-Lists "0";' >> 10periodic
 echo 'APT::Periodic::Download-Upgradeable-Packages "0";' >> 10periodic
 echo 'APT::Periodic::AutocleanInterval "0";' >> 10periodic
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
-
-sudo apt-get update -y
-sudo apt-get install ros-kinetic-desktop-full -y
-
-sudo rosdep init -y
-rosdep update -y
-
-
-# setup environment
-source /opt/ros/kinetic/setup.bash
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-
-# install build-essential
-sudo apt-get install build-essential
-
-# install editors/ ssh
-sudo apt-get install ssh emacs qtcreator vim git -y
-
-# install ros packages and other dependencies
-sudo apt-get install ros-kinetic-sound-play ros-kinetic-rqt-graph ros-kinetic-rqt-gui ros-kinetic-rqt-plot ros-kinetic-kobuki-soft ros-kinetic-kobuki-keyop ros-kinetic-roscpp-tutorials ros-kinetic-librealsense ros-kinetic-rgbd-launch ros-kinetic-cmake-modules ros-kinetic-camera-info-manager -y
-
-sudo apt-get install libboost-random1.58-dev openjdk-7-jre ipython -y
-
-# pip wstool and git
-sudo apt-get install git python-pip -y
-
-sudo pip install wstool
-
-# create catkin ws
-
-mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws/src
-
-catkin_init_workspace
-wstool init
-
-cd ~/catkin_ws
-catkin_make
-echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
-source ~/catkin_ws/devel/setup.bash
-source ~/.bashrc
+wget https://raw.githubusercontent.com/KTH-RAS/ras_install/kinetic-2018/scripts/install_basic.sh
+sh ./install_basic.sh
 
 # install realsense drivers
 cd
