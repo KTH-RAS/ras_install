80423a59163587c76e4860529d8a3302bf768bce
Diogo Almeida <diogoa@kth.se>
2018-02-16 14:23:16 +0100

    Update install_basic.sh

--- scripts/install_basic.sh
+++ scripts/install_basic.sh
@@ -1,4 +1,4 @@
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" >> /etc/apt/sources.list.d/ros-latest.list'
 
 sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
 
